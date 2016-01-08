fcc6c51f7f50ff86e16fb49a5c920eb4565324ba
klhall1987@gmail.com <klhall1987@gmail.com>
2016-01-08 09:54:50 -0500

    Created plugin class and index.

---
+++ custom-frontend-notices.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+/*
+Plugin Name: Custom Frontend Notices
+Description: Allows users to add and manage custom notices for the frontend of their sites.
+Version: 0.0.1
+Author: Kenny Hall
+Author URI: http://kennyinthewild.com
+*/
+
+class Custom_Frontend_Notices
+{
+
+}
