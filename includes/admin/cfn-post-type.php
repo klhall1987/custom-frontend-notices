071b25b78390a7a6cf9897dda2da09f7173c2869
klhall1987@gmail.com <klhall1987@gmail.com>
2016-01-14 14:10:25 -0500

    Removed logic from cfn-settings.php and added taxonomy to cfn-post-type.php

--- includes/admin/cfn-post-type.php
+++ includes/admin/cfn-post-type.php
@@ -5,10 +5,12 @@ class CFN_Post_Type
 
     public function __construct()
     {
-        add_action('init', array( $this, 'cfn_custom_post_type'), 10);
+        add_action( 'init', array( $this, 'cfn_register_post_type' ), 10 );
+
+        add_action( 'init', array( $this, 'cfn_register_taxonomy' ), 10 );
     }
 
-    public function cfn_custom_post_type()
+    public function cfn_register_post_type()
     {
 
         $labels = array(
@@ -42,7 +44,7 @@ class CFN_Post_Type
             'description' => __('Custom frontend notices post type', 'text_domain'),
             'labels' => $labels,
             'supports' => array(),
-            'taxonomies' => array( 'post_tag' ),
+            'taxonomies' => array( 'cfn_post_type' ),
             'hierarchical' => false,
             'public' => true,
             'show_ui' => true,
@@ -54,9 +56,45 @@ class CFN_Post_Type
             'has_archive' => true,
             'exclude_from_search' => false,
             'publicly_queryable' => true,
-            'capability_type' => 'page',
+            'capability_type' => 'post',
         );
         register_post_type('cfn_post_type', $args);
+
+    }
+
+    public function cfn_register_taxonomy()
+    {
+        $labels = array(
+            'name'                       => _x( 'Custom Notices', 'Custom Notices', 'text_domain' ),
+            'singular_name'              => _x( 'Custom Notice', 'Custom Notice', 'text_domain' ),
+            'menu_name'                  => __( 'Custom Notices', 'text_domain' ),
+            'all_items'                  => __( 'All Custom Notices', 'text_domain' ),
+            'parent_item'                => __( 'Parent Custom Notices', 'text_domain' ),
+            'parent_item_colon'          => __( 'Parent Custom Notices:', 'text_domain' ),
+            'new_item_name'              => __( 'New Custom Notice', 'text_domain' ),
+            'add_new_item'               => __( 'Add New Custom Notice', 'text_domain' ),
+            'edit_item'                  => __( 'Edit Custom Notice', 'text_domain' ),
+            'update_item'                => __( 'Update Custom Notice', 'text_domain' ),
+            'view_item'                  => __( 'View Custom Notice', 'text_domain' ),
+            'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+            'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+            'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+            'popular_items'              => __( 'Popular Custom Notices', 'text_domain' ),
+            'search_items'               => __( 'Search Custom Notices', 'text_domain' ),
+            'not_found'                  => __( 'Not Found', 'text_domain' ),
+        );
+
+        $args = array(
+            'labels'                     => $labels,
+            'hierarchical'               => false,
+            'public'                     => true,
+            'show_ui'                    => true,
+            'show_admin_column'          => true,
+            'show_in_nav_menus'          => true,
+            'show_tagcloud'              => true,
+        );
+
+        register_taxonomy( 'Custom Notice', 'cfn_post_type', $args );
 
     }
 }
--- includes/cfn-settings.php
+++ includes/cfn-settings.php
@@ -2,24 +2,9 @@
 
 class CNF_Settings
 {
-    public $args;
-
-    public $query;
-
-    public function __construct()
-    {
-        $this->args = array( 'post_type' => 'page' );
-
-        $this->query = new WP_Query( $this->args );
-    }
-
-    public function getQuery()
-    {
-        return $this->query;
-    }
 }
 
-return new CNF_Settings();
+
 
 
 
