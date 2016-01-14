ff6a0d9947a9672f28788ed8424eb5d83e165ae7
klhall1987@gmail.com <klhall1987@gmail.com>
2016-01-14 15:30:27 -0500

    Setting get posts and creating the args array for getting the taxonomy.

--- includes/cfn-settings.php
+++ includes/cfn-settings.php
@@ -2,9 +2,36 @@
 
 class CNF_Settings
 {
+    public $posts_array;
+
+    public $args = array();
+
+    public function __construct()
+    {
+        $this->args = array(
+            'posts_per_page' => 1,
+            'offset' => 0,
+            'category' => 'Custom Notice',
+            'category_name' => 'Custom Notices',
+            'orderby' => 'date',
+            'order' => 'DESC',
+            'include' => '',
+            'exclude' => '',
+            'meta_key' => '',
+            'meta_value' => '',
+            'post_type' => 'cfn_post_type',
+            'post_mime_type' => '',
+            'post_parent' => '',
+            'author' => '',
+            'post_status' => 'publish',
+            'suppress_filters' => true
+        );
+
+        $this->posts_array = get_posts( $this->args );
+    }
+
 }
 
-
-
+return new CNF_Settings();
 
 
